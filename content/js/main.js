a41cc9a41685ebed7cf99753e4abe7bf5951b6c2
Sergi Zamora <szamora@bvirtual.com>
2018-11-28 13:41:31 +0100

    test

--- content/js/main.js
+++ content/js/main.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
   initParallax();
-  
-  $('.frame').each(function() {
-    $(this).addClass('start');
-  });
-  $("#ball").addClass('start');
+  $("#container").addClass("start");
+ // $("#ball").addClass('start');
   setTimeout(function(){
-    snowStorm.start();
+    //snowStorm.start();
   },1500)
 });
 
 function initParallax(){
-	var scene = document.getElementById('scene');
-	var parallaxInstance = new Parallax(scene);
+  var scene = $('#scene').get(0);
+  var parallaxInstance = new Parallax(scene,{
+    pointerEvents: true
+  });
 }
 
 
